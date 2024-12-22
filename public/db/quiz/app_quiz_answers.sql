1efeaac925c85515e5c9598b497daa5a21ac2e5e
Hany Darwish <hany.freestyle4u@gmail.com>
2024-12-22 22:12:39 +0200

    update

--- public/db/quiz/app_quiz_answers.sql
+++ public/db/quiz/app_quiz_answers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2024 at 09:09 PM
+-- Generation Time: Dec 22, 2024 at 09:11 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
